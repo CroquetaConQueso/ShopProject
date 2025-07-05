5c7868e3312588eefecfd6a0635168c035527489
CroquetaConQueso <lacio161@gmail.com>
2025-07-05 11:01:41 +0200

    Another problem with gitignore

--- src/main/java/com/proyectotienda/controller/ProductController.java
+++ src/main/java/com/proyectotienda/controller/ProductController.java
@@ -1,1 +1,29 @@
+package com.proyectotienda.controller;
+
 //It will control the flow of the creation and the connectivity of all the other Product classes
+import com.proyectotienda.model.Product;
+import com.proyectotienda.repository.ProductDAO;
+
+public class ProductController {
+    private final ProductDAO productDAO;
+    private final ProductInputHandler productInputHandler;
+
+    public ProductController(ProductDAO productDAO, ProductInputHandler productInputHandler) {
+        this.productDAO = productDAO;
+        this.productInputHandler = productInputHandler;
+    }
+
+    public Product createProduct() {
+        Product product = Product.builder().productName(productInputHandler.getValidatedName())
+                .productType(productInputHandler.getValidatedType())
+                .productPrice(productInputHandler.getValidatedPrice()).build();
+        boolean check = productDAO.addProduct(product);
+
+        if(!check){
+            System.out.println("Error while trying to add a product to the database");
+            return null;
+        }
+
+        return product;
+    }
+}
--- src/main/java/com/proyectotienda/controller/UserController.java
+++ src/main/java/com/proyectotienda/controller/UserController.java
@@ -1,1 +1,27 @@
+package com.proyectotienda.controller;
+
 //It will control the flow of the creation and the connectivity of all the other User classes
+import com.proyectotienda.model.User;
+import com.proyectotienda.repository.UserDAO;
+
+public class UserController {
+    private final UserDAO userDAO;
+    private final UserInputHandler userInputHandler;
+
+    public UserController(UserDAO userDAO, UserInputHandler userInputHandler) {
+        this.userDAO = userDAO;
+        this.userInputHandler = userInputHandler;
+    }
+
+    public User createUser() {
+        User user = User.builder().userName(userInputHandler.getValidatedName())
+                .userPass(userInputHandler.getValidatedPassword()).userFunds(userInputHandler.getValidatedFunds())
+                .build();
+        boolean check = userDAO.addUser(user);
+        if(!check){
+            System.out.println("There was an error adding the user to the database");
+            return null;
+        }
+        return user;
+    }
+}
