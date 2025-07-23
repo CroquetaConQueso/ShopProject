34dfed2e1d5a8eed8ce3117792222bcbaa4ac0ab
CroquetaConQueso <lacio161@gmail.com>
2025-07-23 20:27:25 +0200

    Added SceneControllerCard, Changes on Menu, productDAO

---
+++ src/main/java/com/proyectotienda/controller/SceneControllerCard.java
@@ -0,0 +1,27 @@
+package com.proyectotienda.controller;
+
+import com.proyectotienda.model.Product;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class SceneControllerCard{
+    private Product product;
+
+    @FXML
+    private Label labelProductName;
+    @FXML
+    private Label labelProductPrice;
+    //Establishes the setter
+    public void setProduct(Product p){
+        product = p;
+    }
+    //Gives value to the labels of the product card
+    public void giveValuesCard(){
+        if(product != null){
+            labelProductName.setText(product.getProductName());
+            labelProductPrice.setText(String.valueOf(product.getProductPrice()));
+        }
+    }
+
+}
